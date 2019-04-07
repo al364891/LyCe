05b659e7c86ef8366863039c720592397c224901
marklg5 <43933460+marklg5@users.noreply.github.com>
2019-04-07 19:27:39 +0200

    Menú Principal con fade in y fade out hechos

---
+++ Proyecto LyCe/Assets/Scripts/Others/HudBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudBehaviour : MonoBehaviour {
+
+    public Image blackScreen;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(FadeIn(1.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator FadeIn(float time)
+    {
+        Color initialColor = blackScreen.GetComponent<Image>().color;
+        Color c = initialColor;
+        blackScreen.gameObject.SetActive(true);
+        Color finalColor = new Color(0, 0, 0, 0);
+        float elapsedTime = 0.0f;
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            c = Color.Lerp(initialColor, finalColor, elapsedTime / time);
+            blackScreen.GetComponent<Image>().color = c;
+            yield return null;
+        }
+        blackScreen.GetComponent<Image>().color = finalColor;
+    }
+}
---
+++ Proyecto LyCe/Assets/Scripts/Others/MainMenuBehaviour.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehaviour : MonoBehaviour {
+
+    public Button newGame, loadGame, options, exit, volumen, controles, atrasOpciones, atrasVolumen, atrasControles;
+    public Slider volumenControl;
+
+    public Image blackScreen;
+
+	// Use this for initialization
+	void Start () {
+        // Aqui podemos usar un DontDestroyOnLoad para conservar el audio manager
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void newGameFunc()
+    {
+        Debug.Log("newGame");
+        StartCoroutine(FadeOut(1.0f));
+    }
+
+    public void loadGameFunc()
+    {
+        //Codigo para cargar partida
+    }
+
+    public void optionsFunc()
+    {
+        newGame.gameObject.SetActive(false);
+        loadGame.gameObject.SetActive(false);
+        options.gameObject.SetActive(false);
+        exit.gameObject.SetActive(false);
+        volumen.gameObject.SetActive(true);
+        controles.gameObject.SetActive(true);
+        atrasOpciones.gameObject.SetActive(true);
+    }
+
+    public void exitFunc()
+    {
+        Application.Quit();
+    }
+
+    public void volumenFunc()
+    {
+        atrasOpciones.gameObject.SetActive(false);
+        controles.gameObject.SetActive(false);
+        volumen.gameObject.SetActive(false);
+        volumenControl.gameObject.SetActive(true);
+        atrasVolumen.gameObject.SetActive(true);
+    }
+
+    public void cambiarVolumen()
+    {
+        //AudioManager.Instance.ChangeGeneralVolume(volumenControl.value);
+    }
+
+    public void controlesFunc()
+    {
+        controles.gameObject.SetActive(false);
+        volumen.gameObject.SetActive(false);
+        atrasOpciones.gameObject.SetActive(false);
+        atrasControles.gameObject.SetActive(true);
+    }
+
+    public void atrasOpcionesFunc()
+    {
+        newGame.gameObject.SetActive(true);
+        loadGame.gameObject.SetActive(true);
+        options.gameObject.SetActive(true);
+        exit.gameObject.SetActive(true);
+        volumen.gameObject.SetActive(false);
+        controles.gameObject.SetActive(false);
+        atrasOpciones.gameObject.SetActive(false);
+    }
+
+    public void atrasVolumenFunc()
+    {
+        atrasOpciones.gameObject.SetActive(true);
+        controles.gameObject.SetActive(true);
+        volumen.gameObject.SetActive(true);
+        volumenControl.gameObject.SetActive(false);
+        atrasVolumen.gameObject.SetActive(false);
+    }
+
+    public void atrasControlesFunc()
+    {
+        controles.gameObject.SetActive(true);
+        volumen.gameObject.SetActive(true);
+        atrasOpciones.gameObject.SetActive(true);
+        atrasControles.gameObject.SetActive(false);
+    }
+
+    IEnumerator FadeOut(float time)
+    {
+        Color initialColor = blackScreen.GetComponent<Image>().color;
+        Color c = initialColor;
+        blackScreen.gameObject.SetActive(true);
+        Color finalColor = new Color(0, 0, 0, 1);
+        float elapsedTime = 0.0f;
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            c = Color.Lerp(initialColor, finalColor, elapsedTime / time);
+            blackScreen.GetComponent<Image>().color = c;
+            yield return null;
+        }
+        blackScreen.GetComponent<Image>().color = finalColor;
+        SceneManager.LoadScene(0);
+    }
+}
