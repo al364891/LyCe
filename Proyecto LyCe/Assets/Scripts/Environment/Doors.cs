3a913735ff8c65d97bf5164c98fd97beae39150c
jandrit <al361652@uji.es>
2019-04-12 00:52:05 +0200

    Puertas arregladas y collider para activar la puerta del coche puesto
    Lo otro lo haré entre mañana y pasado que no estoy en mi plena capacidad mental sorry

--- Proyecto LyCe/Assets/Scripts/Environment/Doors.cs
+++ Proyecto LyCe/Assets/Scripts/Environment/Doors.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 public class Doors : MonoBehaviour {
 
     public enum Door {childDoor, pasilloChildDoor, banyo2Planta2, sisterDoor, parentsDoor, banyo1Planta2, recibidorDerecha, recibidorIzquierda, puertaSalon, puertaCocina, banyoPlanta1, puertaGaraje1, puertaGaraje2, ladder }
-    private bool opened = false;
+    private bool opened = false, moving = false;
     public Door whatDoorAmI;
+    private float count = 0.0f;
 
     public Animator anim;
+    private Collider coll;
+
+    void Start()
+    {
+        coll = GetComponent<Collider>();
+    }
+
+    void FixedUpdate()
+    {
+        if (moving)
+        {
+            count += Time.deltaTime;
+            if (count > 1.0f)
+            {
+                moving = false;
+                coll.enabled = true;
+            }
+        } else
+        {
+            count = 0.0f;
+        }
+    }
 
     public void controlDoor()
     {
@@ -21,6 +44,8 @@ public class Doors : MonoBehaviour {
             opened = false;
             anim.SetBool("Abierto", opened);
         }
+        moving = true;
+        coll.enabled = false;
     }
 
     public void controlLadder()
