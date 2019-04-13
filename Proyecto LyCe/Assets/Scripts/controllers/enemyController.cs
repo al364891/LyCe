f75d322c520838134e9231dbdad534a8a4bfdbad
al364891 <al364891@ui.es>
2019-04-13 20:45:43 +0200

    IA primeros pasos

---
+++ Proyecto LyCe/Assets/Scripts/controllers/enemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyController : MonoBehaviour {
+    /*
+    public float lookRadious = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        target = playerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadious)
+        {
+            agent.SetDestination(target.position);
+        }
+	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadious);
+    }
+    */
+}
