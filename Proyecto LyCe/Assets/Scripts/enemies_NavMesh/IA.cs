b9c2185224eb615e1ee1374d13972b4e59b1534e
al364891 <44838961+al364891@users.noreply.github.com>
2019-04-22 21:02:53 +0200

    vida + daño + arreglos IA

--- Proyecto LyCe/Assets/Scripts/enemies_NavMesh/IA.cs
+++ Proyecto LyCe/Assets/Scripts/enemies_NavMesh/IA.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class prueba : MonoBehaviour {
+public class IA : MonoBehaviour
+{
     //ESTE SCRIPT: sistema automatizado de "puntos donde irá el enemigo"
     //simplemente crear un GameObject como punto de referencia de donde quieres ir
     //y pasarlo a lista "puntos" de este script
 
-    public NavMeshAgent padre;
+    public UnityEngine.AI.NavMeshAgent padre;
     public List<GameObject> puntos;
     List<Vector3> puntosUtiles;
     Vector3 puntosTemp;
@@ -16,30 +16,31 @@ public class prueba : MonoBehaviour {
     //int p;
     float posPadreX, posPadreZ;
     float posX, posZ;
-    
-    Vector3 pDestino;
+
+    public Vector3 pDestino;
     Vector3 pAux;
-    
+
 
     void Start ()
     {
         puntosUtiles = new List<Vector3>();
-        
-        for (int i=0; i<puntos.Count; i++)
+
+        for (int i = 0; i < puntos.Count; i++)
         {
-            puntosTemp = (Vector3) puntos[i].transform.position;
+            puntosTemp = (Vector3)puntos[i].transform.position;
             puntosUtiles.Add(puntos[i].transform.position);
         }
 
         pDestino = puntosUtiles[0];
         posX = pDestino.x;
         posZ = pDestino.z;
-        
+
         padre.SetDestination(pDestino);
     }
+	
 
-
-	void Update () {
+	void Update ()
+    {
         posPadreX = padre.transform.position.x;
         posPadreZ = padre.transform.position.z;
 
@@ -55,6 +56,15 @@ public class prueba : MonoBehaviour {
             posZ = pDestino.z;
 
             padre.SetDestination(pDestino);
+
+            /*Debug.Log("LISTA:"+puntosUtiles+"\n");
+            for(int i=0; i<puntosUtiles.Count; i++)
+            {
+                Debug.Log("++++++++++++++++++");
+                Debug.Log("punto["+i+"]X: " + puntosUtiles[i].x);
+                Debug.Log("punto[" + i + "]Y: " + puntosUtiles[i].y);
+                Debug.Log("punto[" + i + "]Z: " + puntosUtiles[i].z);
+            }*/
         }
     }
 }
--- Proyecto LyCe/Assets/Scripts/enemies_NavMesh/enemyController.cs
+++ Proyecto LyCe/Assets/Scripts/enemies_NavMesh/enemyController.cs
@@ -5,14 +5,26 @@ using UnityEngine.AI;
 
 public class enemyController : MonoBehaviour {
     //ESTE SCRIPT: es para el radio en el que te puede "ver" el padre
-    
+
+    IA IA;
+    public GameObject enemy;
+    life_player life_player;
+    public GameObject player;
+
     public float lookRadious = 10f;
+    public int damage;
+    public int wait_seconds;
+    bool salida = true;
 
     Transform target;
     NavMeshAgent agent;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        IA = enemy.GetComponent<IA>();
+        life_player = player.GetComponent<life_player>();
+
         target = playerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
 	}
@@ -23,7 +35,38 @@ public class enemyController : MonoBehaviour {
 
         if (distance <= lookRadious)
         {
-            agent.SetDestination(target.position);
+            if(distance > 5)
+            {
+                if (agent.stoppingDistance == 5){
+                    agent.stoppingDistance = 0;
+                }
+                agent.SetDestination(target.position);
+            }
+            else
+            {
+                agent.stoppingDistance = 5;
+
+                if (life_player.life >= 0)
+                {
+                    if (salida)
+                    {
+                        /*life_player.life -= Time.deltaTime;
+                        Debug.Log("vida: " + life_player.life);*/
+                        //Damage();
+                        StartCoroutine(Damage_enemy());
+                        Debug.Log("vida: " + life_player.life);
+                    }
+                }
+                else
+                {
+                    Debug.Log("has perdido");
+                }
+            }
+            
+        }
+        else
+        {
+            IA.padre.SetDestination(IA.pDestino);
         }
 	}
 
@@ -33,4 +76,11 @@ public class enemyController : MonoBehaviour {
         Gizmos.DrawWireSphere(transform.position, lookRadious);
     }
     
+    IEnumerator Damage_enemy()
+    {
+        life_player.life -= damage;
+        salida = false;
+        yield return new WaitForSeconds(wait_seconds);
+        salida = true;
+    }
 }
